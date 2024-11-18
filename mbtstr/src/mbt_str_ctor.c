7402ab5e813ea4288e95c119148f0ed80483f962
Jules Magnan <jules.magnan@epita.fr>
2024-11-18 23:23:27 +0100

    [ADD] function mbt_str_ctor

---
+++ mbtstr/src/mbt_str_ctor.c
@@ -0,0 +1,14 @@
+#include "mbtstr/str.h"
+#include "stdlib.h"
+
+bool mbt_str_ctor(struct mbt_str *str, size_t capacity)
+{
+    char *data = calloc(capacity, sizeof(char));
+    if (data == NULL)
+    {
+        return false;
+    }
+    str->data = data;
+    str->capacity = capacity;
+    return true;
+}
