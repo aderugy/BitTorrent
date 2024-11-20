5bee19cbb664525ee47cee21a581953e8a87789c
Arthur de Rugy <arthur.goullet-de-rugy@epita.fr>
2024-11-20 01:17:27 +0100

    fix: multiple clients

--- epoll_server/epoll_server.c
+++ epoll_server/epoll_server.c
@@ -1,13 +1,20 @@
 #include "epoll_server.h"
 
+#include <err.h>
+#include <errno.h>
 #include <netdb.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <sys/epoll.h>
 #include <sys/socket.h>
 #include <unistd.h>
 
 #include "connection.h"
 #include "utils/xalloc.h"
+
+#define MAX_CONCURRENT_IO 10
+#define BUFFER_SIZE 1000
 
 /**
  * \brief Iterate over the struct addrinfo elements to create and bind a socket
@@ -39,7 +46,7 @@ int create_and_bind(struct addrinfo *addrinfo)
         addrinfo = addrinfo->ai_next;
     }
 
-    return 0;
+    errx(EXIT_FAILURE, "create_and_bind");
 }
 
 /**
@@ -58,17 +65,23 @@ int create_and_bind(struct addrinfo *addrinfo)
 int prepare_socket(const char *ip, const char *port)
 {
     struct addrinfo *addrinfo = NULL;
-
-    char *hostname;
-    asprintf(&hostname, "%s:%s", ip, port);
-
-    if (getaddrinfo(hostname, NULL, NULL, &addrinfo))
+    if (getaddrinfo(ip, port, NULL, &addrinfo))
     {
-        return -1;
+        errx(1, "getaddrinfo");
     }
 
     int s_fd = create_and_bind(addrinfo);
     freeaddrinfo(addrinfo);
+
+    if (s_fd <= 0)
+    {
+        errx(1, "prepare_socket");
+    }
+
+    if (listen(s_fd, 10))
+    {
+        errx(1, "listen");
+    }
 
     return s_fd;
 }
@@ -92,15 +105,102 @@ struct connection_t *accept_client(int epoll_instance, int server_socket,
         return NULL;
     }
 
-    struct epoll_event *event = xcalloc(1, sizeof(struct epoll_event));
-    event->events = EPOLLIN | EPOLLOUT;
-    epoll_ctl(epoll_instance, EPOLL_CTL_ADD, c_fd, event);
+    struct epoll_event event = { .events = EPOLLIN, .data = { .fd = c_fd } };
+    if (epoll_ctl(epoll_instance, EPOLL_CTL_ADD, c_fd, &event) == -1)
+    {
+        errx(EXIT_FAILURE, "epoll_ctl");
+    }
 
     struct connection_t *next = add_client(connection, c_fd);
     if (!next)
     {
-        epoll_ctl(epoll_instance, EPOLL_CTL_DEL, c_fd, NULL);
+        errx(EXIT_FAILURE, "add_client");
     }
 
     return next;
+}
+
+int main(int argc, char *argv[])
+{
+    if (argc != 3)
+    {
+        return 1;
+    }
+
+    char *ip = argv[1];
+    char *port = argv[2];
+
+    int epoll_fd = epoll_create1(0);
+    if (epoll_fd == -1)
+    {
+        return 1;
+    }
+
+    int socket_fd = prepare_socket(ip, port);
+    if (socket_fd <= 0)
+    {
+        errx(EXIT_FAILURE, "prepare_socket");
+    }
+
+    struct epoll_event event = { .events = EPOLLIN,
+                                 .data = { .fd = socket_fd } };
+    if (epoll_ctl(epoll_fd, EPOLL_CTL_ADD, socket_fd, &event) == -1)
+    {
+        errx(EXIT_FAILURE, "create epoll");
+    }
+
+    struct connection_t *clients = NULL;
+    while (42)
+    {
+        struct epoll_event events[MAX_CONCURRENT_IO];
+
+        int nb_fd = epoll_wait(epoll_fd, events, MAX_CONCURRENT_IO, -1);
+        if (nb_fd == -1)
+        {
+            errx(EXIT_FAILURE, "epoll_wait");
+        }
+
+        for (int i = 0; i < nb_fd; i++)
+        {
+            struct epoll_event current = events[i];
+            int client_fd = current.data.fd;
+
+            struct connection_t *client = find_client(clients, client_fd);
+            if (!client)
+            {
+                client = accept_client(epoll_fd, socket_fd, clients);
+                clients = client;
+
+                if (!client)
+                {
+                    errx(EXIT_FAILURE, "Unknown fd");
+                }
+            }
+
+            char buffer[BUFFER_SIZE + 1];
+            int r = recv(client->client_socket, buffer, BUFFER_SIZE, 0);
+
+            if (r <= 0)
+            {
+                printf("Removing client\n");
+                epoll_ctl(epoll_fd, EPOLL_CTL_DEL, client->client_socket, NULL);
+                clients = remove_client(clients, client->client_socket);
+            }
+            else
+            {
+                buffer[r] = 0;
+                printf("%s", buffer);
+            }
+        }
+    }
+
+    close(epoll_fd);
+    close(socket_fd);
+
+    while (clients)
+    {
+        int fd = clients->client_socket;
+        clients = remove_client(clients, fd);
+        close(fd);
+    }
 }
