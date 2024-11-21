6f6ec71c72bf6c4336361ea957c923932c2f3531
Jwles <jules.magnan@epita.fr>
2024-11-21 18:27:44 +0100

    [] mbtbe commence

--- bittorrent/libs/mbtbe/src/mbt_be_parse_torrent_file.c
+++ bittorrent/libs/mbtbe/src/mbt_be_parse_torrent_file.c
@@ -1,5 +1,9 @@
 #include <mbt/be/bencode.h>
 #include <mbt/utils/str.h>
+#include <mbt/utils/view.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <string.h>
 
 #include "mbt/be/torrent.h"
 
@@ -21,9 +25,44 @@ struct info
 
 bool mbt_be_parse_torrent_file(const char *path, struct mbt_torrent *torrent)
 {
-    struct mbt_be *file = mbt_str_init(64);
+    struct mbt_str *data = mbt_str_init(64);
+    if (!mbt_str_ctor(data, 64))
+    {
+        mbt_str_free(data);
+        return false;
+    }
+    if (!mbt_str_read_file(path, data))
+    {
+        mbt_str_free(data);
+        return false;
+    }
 
-    
+    struct mbt_cview *view = calloc(1, sizeof(struct mbt_cview));
+    if (!view)
+    {
+        mbt_str_free(data);
+        return false;
+    }
+    view->data = data->data;
+    view->size = data->size;
+
+    struct mbt_be_node *node = mbt_be_decode(view);
+
+    if (node->type != MBT_BE_DICT)
+    {
+        mbt_str_free(data);
+        free(view);
+        return false;
+    }
+
+    for (size_t i = 0; node->v.dict[i]; i++)
+    {
+        char *key = node->v.dict[i]->key.data;
+        if (strcmp("announce", key) == 0)
+        {
+            torrent->announce = &node->v.dict[i]->val->v.str;
+        }
+    }
 
     return true;
 }
---
+++ bittorrent/libs/mbtutils/src/mbt_str_read_file.c
@@ -0,0 +1,16 @@
+#include "mbt/utils/str.h"
+bool mbt_str_read_file(const char *path, struct mbt_str *str)
+{
+    FILE *file = fopen(path, "r");
+    if (!file)
+    {
+        return false;
+    }
+    char *line = NULL;
+    size_t len = 0;
+    while (getline(&line, &len, file) != -1)
+    {
+        mbt_str_pushcstr(str, line);
+    }
+    return true;
+}
