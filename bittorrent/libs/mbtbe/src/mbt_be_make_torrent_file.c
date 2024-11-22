819ee763286c30ddc53d324ef924e0e32d93b1de
Jwles <jules.magnan@epita.fr>
2024-11-22 03:20:05 +0100

    [] mbtbe bruh2

--- bittorrent/libs/mbtbe/src/mbt_be_make_torrent_file.c
+++ bittorrent/libs/mbtbe/src/mbt_be_make_torrent_file.c
@@ -1,6 +1,8 @@
 #include <mbt/be/torrent.h>
 #include <mbt/utils/str.h>
 #include <stdlib.h>
+#include <sys/stat.h>
+#include <sys/types.h>
 
 struct torrent_file
 {
@@ -8,6 +10,16 @@ struct torrent_file
     struct mbt_str *created_by;
     struct mbt_str *creation_date;
     struct info *info;
+};
+
+struct info
+{
+    struct mbt_str *piece_length;
+    struct mbt_str *pieces;
+    struct mbt_str *name;
+    uint64_t length;
+    struct files *files;
+    struct mbt_str *sha1;
 };
 
 bool init_announce(struct mbt_be_node *node, struct torrent_file *torrent)
@@ -18,6 +30,17 @@ bool init_announce(struct mbt_be_node *node, struct torrent_file *torrent)
     }
     torrent->announce = &node->v.str;
     return true;
+}
+
+uint64_t get_file_size(const char *filename)
+{
+    struct stat st;
+    if (stat(filename, &st) != 0)
+    {
+        perror("stat");
+        exit(EXIT_FAILURE);
+    }
+    return st.st_size;
 }
 
 bool mbt_be_make_torrent_file(const char *path)
@@ -43,11 +66,8 @@ bool mbt_be_make_torrent_file(const char *path)
         return false;
     }
 
-    if (!init_announce(node->v.dict[0]->val, torrent))
-    {
-        mbt_str_free(data);
-        return false;
-    }
+    struct info *info = malloc(sizeof(struct info));
+    info->length = get_file_size(path);
 
     return true;
 }
