cdb8cfe10707024029e6b0cc2a0e4a4fce8a44f3
Jwles <jules.magnan@epita.fr>
2024-11-25 17:00:51 +0100

    [feature] parser et maker finito ?

--- bittorrent/libs/mbtbe/src/mbt_be_make_torrent_file.c
+++ bittorrent/libs/mbtbe/src/mbt_be_make_torrent_file.c
@@ -2,9 +2,11 @@
 #include <mbt/be/torrent.h>
 #include <mbt/be/types_mbtbe.h>
 #include <mbt/utils/file.h>
+#include <mbt/utils/hash.h>
 #include <mbt/utils/str.h>
 #include <pwd.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/stat.h>
 #include <sys/types.h>
 
@@ -38,6 +40,115 @@ uint64_t get_file_size(const char *filename)
         exit(EXIT_FAILURE);
     }
     return st.st_size;
+}
+
+struct mbt_be_pair *get_pieces_length(void)
+{
+    struct mbt_be_node *node = mbt_be_num_init(256 * 1024);
+    struct mbt_be_pair *pair = transform_to_pair(node, "piece length");
+    return pair;
+}
+char *parse_path_get_file_name(const char *path)
+{
+    char *file_name = strrchr(path, '/');
+    if (file_name == NULL)
+    {
+        return NULL;
+    }
+    return file_name + 1;
+}
+struct mbt_be_pair *get_name(const char *path)
+{
+    struct mbt_str name;
+    if (!mbt_str_ctor(&name, 64))
+    {
+        mbt_str_free(&name);
+        return NULL;
+    }
+    if (!mbt_str_pushcstr(&name, parse_path_get_file_name(path)))
+    {
+        mbt_str_free(&name);
+        return NULL;
+    }
+    struct mbt_be_node *node = mbt_be_str_init(MBT_CVIEW_OF(name));
+    struct mbt_be_pair *pair = transform_to_pair(node, "name");
+    mbt_str_dtor(&name);
+    return pair;
+}
+
+struct mbt_be_pair *get_length(const char *path)
+{
+    uint64_t size = get_file_size(path);
+    struct mbt_be_node *node = mbt_be_num_init(size);
+    struct mbt_be_pair *pair = transform_to_pair(node, "length");
+    return pair;
+}
+
+struct mbt_be_pair *get_pieces(const char *path)
+{
+    struct mbt_str data;
+    if (!mbt_str_ctor(&data, 64))
+    {
+        mbt_str_free(&data);
+        return NULL;
+    }
+    if (!mbt_str_read_file(path, &data))
+    {
+        mbt_str_free(&data);
+        return NULL;
+    }
+
+    struct mbt_str sha1_mbt;
+    if (!mbt_str_ctor(&sha1_mbt, 64))
+    {
+        mbt_str_free(&sha1_mbt);
+        return NULL;
+    }
+
+    size_t remaining_size = data.size;
+    for (size_t i = 0; i < data.size; i += 256 * 1024)
+    {
+        if (remaining_size < 256 * 1024)
+        {
+            char *sha1_str = sha1(data.data + i, remaining_size);
+            if (!mbt_str_pushcstr(&sha1_mbt, sha1_str))
+            {
+                mbt_str_free(&sha1_mbt);
+                return NULL;
+            }
+            free(sha1_str);
+            break;
+        }
+        else
+        {
+            char *sha1_str = sha1(data.data + i, 256 * 1024);
+            if (!mbt_str_pushcstr(&sha1_mbt, sha1_str))
+            {
+                mbt_str_free(&sha1_mbt);
+                return NULL;
+            }
+            free(sha1_str);
+        }
+    }
+
+    struct mbt_be_node *node = mbt_be_str_init(MBT_CVIEW_OF(sha1_mbt));
+    struct mbt_be_pair *pair = transform_to_pair(node, "pieces");
+    mbt_str_dtor(&data);
+    mbt_str_dtor(&sha1_mbt);
+    return pair;
+}
+
+struct mbt_be_pair *create_info_dict(const char *path)
+{
+    struct mbt_be_pair **d = calloc(3 + 1, sizeof(struct mbt_be_pair));
+    d[0] = get_pieces_length();
+    d[1] = get_name(path);
+    d[2] = get_pieces(path);
+    d[3] = get_length(path);
+    d[4] = NULL;
+    struct mbt_be_node *node = mbt_be_dict_init(d);
+    struct mbt_be_pair *pair = transform_to_pair(node, "info");
+    return pair;
 }
 
 struct mbt_be_pair *get_creation_date(const char *path)
@@ -135,7 +246,8 @@ bool mbt_be_make_torrent_file(const char *path)
     d[0] = get_announce();
     d[1] = get_user(path);
     d[2] = get_creation_date(path);
-    d[3] = NULL;
+    d[3] = create_info_dict(path);
+    d[4] = NULL;
 
     struct mbt_be_node *node = mbt_be_dict_init(d);
 
