9e27f220506ba3ebe6ada9c9ba79c5f334eea894
Jules Magnan <jules.magnan@epita.fr>
2024-11-29 22:16:11 +0100

    [print and make]

--- bittorrent/src/mktorrent.c
+++ bittorrent/src/mktorrent.c
@@ -1,6 +1,9 @@
 #include "main.h"
+#include "mbt/be/torrent.h"
 
 int main_mktorrent(struct main_options options)
 {
+    mbt_be_make_torrent_file(options.path);
+     
     return options.flags;
 }
--- bittorrent/src/print.c
+++ bittorrent/src/print.c
@@ -1,6 +1,16 @@
 #include "main.h"
+#include "mbt/be/torrent.h"
+#include "mbt/be/types_mbtbe.h"
 
 int main_prettyprint(struct main_options options)
 {
+    struct mbt_torrent *torrent = mbt_torrent_init();
+
+    mbt_be_parse_torrent_file(options.path, torrent);
+
+    mbt_torrent_print(torrent, 1);
+
+    mbt_torrent_free(torrent);
+
     return options.flags;
 }
